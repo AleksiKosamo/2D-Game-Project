326b693d9ff117d21f65678f69e3b5af71be7d06
Aleksi Jere Matias Kosamo <s3koal03@students.osao.fi>
2024-09-27 13:45:36 +0300

    Updated

---
+++ scripts/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var last_direction = Vector2.ZERO
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * 2500
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		if direction.x < 0:
+			last_direction.x = -1
+			$Wizard.play("run_left")
+		elif direction.x > 0:
+			last_direction.x = 1
+			$Wizard.play("run_right")
+		elif direction.y < 0:
+			if last_direction.x < 0:
+				$Wizard.play("run_left")
+			else:
+				$Wizard.play("run_right")
+		elif direction.y > 0:
+			if last_direction.x < 0:
+				$Wizard.play("run_left")
+			else:
+				$Wizard.play("run_right")
+	else:
+		if last_direction.x < 0:
+			$Wizard.play("idle_left")
+		elif last_direction.x > 0:
+			$Wizard.play("idle_right")
