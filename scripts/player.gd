cfa2c2d9151eb31b271a1099c2a93cf3543224c8
Aleksi Jere Matias Kosamo <s3koal03@students.osao.fi>
2024-09-30 13:42:58 +0300

    Updated

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var last_direction = Vector2.ZERO
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * 2500
 	move_and_slide()
