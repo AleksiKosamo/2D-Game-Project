6e99d3914fd5b27c9c40fca09858ff21d943aa08
Aleksi Jere Matias Kosamo <s3koal03@students.osao.fi>
2024-10-07 13:50:48 +0300

    update

--- scripts/mushroom.gd
+++ scripts/mushroom.gd
@@ -1,29 +1,40 @@
 extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/Player")
-@onready var mushroom = $"."
+@onready var mushroom = $"." 
 var last_direction = Vector2.ZERO
 
 func _physics_process(_delta):
-	var direction = player.global_position.y
-	var directionx = player.global_position.x
-	print(direction,",  ",directionx)
+	var player_pos = player.global_position
+	var mushroom_pos = mushroom.global_position
 	
-	if direction > mushroom.global_position.y:
+	# Reset velocity at the start of the process
+	velocity = Vector2.ZERO
+
+	# Calculate velocity based on player position
+	if player_pos.y > mushroom_pos.y:
 		velocity.y = 200
-	else:
+	elif player_pos.y < mushroom_pos.y:
 		velocity.y = -200
-		
-	if directionx > mushroom.global_position.x:
-		velocity.x = 200
-	else:
-		velocity.x = -200
-	move_and_slide()
 	
+	if player_pos.x > mushroom_pos.x:
+		velocity.x = 200
+		last_direction = Vector2.RIGHT
+	elif player_pos.x < mushroom_pos.x:
+		velocity.x = -200
+		last_direction = Vector2.LEFT
+	
+	# Move the character based on the calculated velocity
+	move_and_slide()
+
 	if velocity.length() > 0.0:
-		if mushroom.global_position.x > directionx:
-			%Mushroom.play("run_left")
-		elif mushroom.global_position.x < directionx:
-			%Mushroom.play("run_right")
-			
-			
+		print(player_pos.x, ", ", mushroom_pos.x)
+		
+		# Only play the run animation if not directly aligned on the x-axis
+		if abs(mushroom_pos.x - player_pos.x) >= 10:
+			%Mushroom.play("run_left" if last_direction == Vector2.LEFT else "run_right")
+		else:
+			# If very close in x but not moving, ensure we don't play running animation
+			%Mushroom.play("idle_right" if last_direction == Vector2.RIGHT else "idle_left")
+	else:
+		%Mushroom.play("attack_right" if last_direction == Vector2.RIGHT else "attack_left")
